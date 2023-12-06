e3d0a4fa8822b84762169cdd7605bcff98fe1fba
HyonHyon <khm3626@gmail.com>
2023-12-06 16:03:27 +0900

    first commit

---
+++ src/test/java/com/example/teamtest2/TeamTest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teamtest2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamTest2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
